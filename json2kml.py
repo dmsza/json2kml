10f779b5e8c1d3152211381af5617f6f0e6eeadc
dmrsouza <dimitri.souza@gmail.com>
2017-02-09 10:58:53 -0200

    Add files via upload

--- json2kml.py
+++ json2kml.py
@@ -1,3 +1,13 @@
+#####################################################################################
+# Python script to convert Google Maps JSON file to a KML file
+# Copyright (C) 2017 Dimitri Souza
+#####################################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#####################################################################################
+   
 import json
 import simplekml
 
