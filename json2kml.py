4f4a8bfc2c35aa00be2910def7462e59f814caf9
dmrsouza <dimitri.souza@gmail.com>
2017-02-09 11:10:34 -0200

    Update json2kml.py

--- json2kml.py
+++ json2kml.py
@@ -4,8 +4,8 @@
 # https://github.com/dmrsouza/json2kml
 #####################################################################################
 # This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation version 3
+# it under the terms of the GNU General Public License v3 as published by
+# the Free Software Foundation.
 #####################################################################################
    
 import json
