58e2c3b37a4016907184bb75beaae883606e7fbe
drmsouza <dimitri.souza@gmail.com>
2017-02-10 11:16:36 -0200

    First version

---
+++ json2csv.py
@@ -0,0 +1,44 @@
+#####################################################################################
+# Python script to convert Google Maps JSON file to a CSV file
+# Copyright (C) 2017 Dimitri Souza
+# https://github.com/dmrsouza/json2kml
+#####################################################################################
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License v3 as published by
+# the Free Software Foundation.
+#####################################################################################
+   
+import json
+import csv
+
+inputFile = "Saved Places.json"
+outputFile = "Saved Places.csv"
+
+print ('Opening input file "'+inputFile+'"')
+with open (inputFile) as jsonFile:
+    data = json.load (jsonFile)
+    
+print ('Creating output file "'+outputFile+'"')   
+with open(outputFile, 'wt') as csvfile:
+    csvWriter = csv.writer(csvfile,quoting=csv.QUOTE_ALL,lineterminator='\n')
+    csvWriter.writerow(['Latitude', 'Longitude', 'Name', 'Address'])
+    
+    count = 0
+    for place in data["features"]:
+        if place["geometry"]["type"] == "Point":
+            title = place["properties"]["Title"]
+            print ('Parsing place "'+title+'"')
+            
+            placeLocation = place["properties"]["Location"]
+            lon = place["geometry"]["coordinates"][0]
+            lat = place["geometry"]["coordinates"][1]
+            
+            if "Address" in placeLocation:
+                address = placeLocation ["Address"]
+            else:
+                address = "N/A"
+            
+            csvWriter.writerow ([lon,lat,title,address])
+            count += 1
+        
+print ('Done! Total of '+str(count)+' places saved to the CSV file.')
