cb01785088694ec691f2a032524b067cd344888b
Dimitri Souza <dimitri.souza@gmail.com>
2017-02-27 10:34:42 -0300

    stdout configured to UTF-8

--- json2csv.py
+++ json2csv.py
@@ -10,9 +10,15 @@
    
 import json
 import csv
+import sys
+import codecs
 
 inputFile = "Saved Places.json"
 outputFile = "Saved Places.csv"
+
+# JSON Encoding is UTF-8. Change stdout to UTF-8 to prevent encoding error
+# when calling print titles inside the loop
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 print ('Opening input file "'+inputFile+'"')
 with open (inputFile) as jsonFile:
